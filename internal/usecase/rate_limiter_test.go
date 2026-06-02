c84b8a2e242e4c8da40e765f8e8a0647c6e692a1
adlermo <adlerorteney@gmail.com>
2026-06-02 14:17:00 -0600

    add test cases

--- internal/usecase/rate_limiter_test.go
+++ internal/usecase/rate_limiter_test.go
@@ -31,6 +31,7 @@ func TestShouldAllowRequestBelowLimit(t *testing.T) {
 	if !allowed {
 		t.Fatal("request should be allowed")
 	}
+
 }
 
 func TestShouldBlockRequestAboveLimit(t *testing.T) {
@@ -63,6 +64,7 @@ func TestShouldBlockRequestAboveLimit(t *testing.T) {
 	if !blocked {
 		t.Fatal("ip should be blocked")
 	}
+
 }
 
 func TestTokenShouldOverrideIPLimit(t *testing.T) {
@@ -108,4 +110,96 @@ func TestTokenShouldOverrideIPLimit(t *testing.T) {
 	if allowed {
 		t.Fatal("token should be blocked after reaching its own limit")
 	}
+
+}
+
+func TestShouldDenyBlockedClient(t *testing.T) {
+	repo := NewFakeRepository()
+
+	limiter := usecase.NewRateLimiter(
+		repo,
+		1,
+		nil,
+		time.Minute,
+	)
+
+	ctx := context.Background()
+
+	_, _ = limiter.Allow(ctx, "127.0.0.1", "")
+	_, _ = limiter.Allow(ctx, "127.0.0.1", "")
+
+	allowed, err := limiter.Allow(ctx, "127.0.0.1", "")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if allowed {
+		t.Fatal("blocked client should remain blocked")
+	}
+
+}
+
+func TestUnknownTokenShouldUseIpLimit(t *testing.T) {
+	repo := NewFakeRepository()
+
+	limiter := usecase.NewRateLimiter(
+		repo,
+		2,
+		map[string]int{
+			"premium-token": 100,
+		},
+		time.Minute,
+	)
+
+	ctx := context.Background()
+
+	_, _ = limiter.Allow(ctx, "127.0.0.1", "unknown-token")
+	_, _ = limiter.Allow(ctx, "127.0.0.1", "unknown-token")
+
+	allowed, err := limiter.Allow(
+		ctx,
+		"127.0.0.1",
+		"unknown-token",
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if allowed {
+		t.Fatal("unknown token should use ip limit")
+	}
+
+}
+
+func TestDifferentIpsShouldHaveIndependentLimits(t *testing.T) {
+	repo := NewFakeRepository()
+
+	limiter := usecase.NewRateLimiter(
+		repo,
+		1,
+		nil,
+		time.Minute,
+	)
+
+	ctx := context.Background()
+
+	_, _ = limiter.Allow(ctx, "127.0.0.1", "")
+	_, _ = limiter.Allow(ctx, "127.0.0.1", "")
+
+	allowed, err := limiter.Allow(
+		ctx,
+		"192.168.1.1",
+		"",
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !allowed {
+		t.Fatal("different ip should not be affected")
+	}
+
 }
