b27a48206dff1c9c794219cdc2a67bf9790952ad
adlermo <adlerorteney@gmail.com>
2026-06-02 01:35:48 -0600

    create gomod and rate_limiter usecase

---
+++ go.mod
@@ -0,0 +1,3 @@
+module fullcycle-rate-limiter
+
+go 1.26.2
---
+++ internal/usecase/rate_limiter.go
@@ -0,0 +1,87 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"fullcycle-rate-limiter/internal/gateway"
+)
+
+type RateLimiter struct {
+	repo gateway.RateLimiterRepository
+
+	ipLimit       int
+	tokenLimits   map[string]int
+	blockDuration time.Duration
+}
+
+func NewRateLimiter(
+	repo gateway.RateLimiterRepository,
+	ipLimit int,
+	tokenLimits map[string]int,
+	blockDuration time.Duration,
+) *RateLimiter {
+	return &RateLimiter{
+		ipLimit:       ipLimit,
+		tokenLimits:   tokenLimits,
+		blockDuration: blockDuration,
+	}
+}
+
+func (r *RateLimiter) Allow(
+	ctx context.Context,
+	ip string,
+	token string,
+) (bool, error) {
+
+	var (
+		key   string
+		limit int
+	)
+
+	if token != "" {
+		if tokenLimit, ok := r.tokenLimits[token]; ok {
+			key = "token:" + token
+			limit = tokenLimit
+		}
+	}
+
+	if key == "" {
+		key = "ip:" + ip
+		limit = r.ipLimit
+	}
+
+	blocked, err := r.repo.IsBlocked(ctx, key)
+	if err != nil {
+		return false, err
+	}
+
+	if blocked {
+		return false, nil
+	}
+
+	count, err := r.repo.Increment(
+		ctx,
+		key,
+		time.Second,
+	)
+	if err != nil {
+		return false, err
+	}
+
+	if count > int64(limit) {
+		err = r.repo.Block(
+			ctx,
+			key,
+			r.blockDuration,
+		)
+
+		if err != nil {
+			return false, err
+		}
+
+		return false, nil
+	}
+
+	return true, nil
+}
